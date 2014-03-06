2ada068a907b44e828c64e5e2e1e9a47186de627
jehob <jehob@web.de>
2014-03-06 12:41:21 +0100

    0.0.3 Test

---
+++ Games/Launcher.Tests/RisikoTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using Games;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Risiko;
+
+namespace Launcher.Tests
+{
+    [TestClass]
+    public class RisikoTests
+    {
+        [TestMethod]
+        public void PointInPolygon_PositivePoint_True()
+        {
+            //var a = new Games.LauncherControl(new Games.Launcher());
+        }
+    }
+}
